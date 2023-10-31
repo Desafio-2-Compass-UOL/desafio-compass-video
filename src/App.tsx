8f23c33bc1fd9e9933fa45f5b9c4771d82e7d16f
Genário Azevedo <genario.azevedo@ufape.edu.br>
2023-10-31 20:19:45 -0300

    feat(add): Headear

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
    <div className="App">
     <h2>Movie</h2>
+    <Header />
    </div>
   )
 }
