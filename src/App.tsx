a3dbc16e4f65dbacef7990673c372687846c8da3
Genário Azevedo <genario.azevedo@ufape.edu.br>
2023-11-07 10:01:07 -0300

    feat(add): routes

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,68 @@ import MoviesPage from './pages/MoviesPage'
 import Home from './pages/Home'
 
 function App() {
-
   return (
-   <div className="App">
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='home' element={<Home/>} />
-        <Route path='filmes' element={<MoviesPage/>} />
-        {/* <Route path='celebridades' element={<Celebridades/>} /> */}
-        <Route path='searchresultspage' element={<SearchResultsPage />} />
-        <Route path='mylistpage' element={<MyListPage />} />
-        <Route path='collectionpage' element={<HalloweenCollectionPage/>} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
-   </div>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='home'
+            element={
+              <>
+                <Header />
+                <Home/>
+                <Footer />
+              </>     
+          } />
+          <Route path='moviepage'
+            element={
+              <>
+              <Header />
+                <MoviesPage />
+              <Footer /> </>
+            }/>
+        {/*<Route path='seriespage'
+            element={
+              <>
+              <Header />
+                <SeriesPage />
+              <Footer /> </>
+            }/>
+            <Route path='celebrities'
+            element={
+              <>
+              <Header />
+                <CelebritiesPage />
+              <Footer /> </>
+            }/> */}
+          <Route path='searchresultspage' 
+            element={
+              <>
+              <Header />
+              <SearchResultsPage />
+              <Footer />
+            </> 
+          } />
+          <Route path='mylistpage' 
+            element={
+              <>
+              <Header />
+              <MyListPage /> 
+              <Footer />
+            </> 
+          } />
+          <Route path='collectionpage' 
+            element={
+            <>
+              <Header />
+              <HalloweenCollectionPage />
+              <Footer />
+            </> 
+          } />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
+
 export default App;
